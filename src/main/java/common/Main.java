bdae640f5f59e34362829cc52d15265c738fe8bc
DiTotto <luca.ditotto99@gmail.com>
2025-01-07 09:41:03 +0100

    resolve code smell, insert main.java in a package

--- src/main/java/common/Main.java
+++ src/main/java/common/Main.java
@@ -25,10 +25,10 @@ public class Main {
     public static void main(String[] args) {
         try {
             System.out.println("Starting...");
-            //String project = "BOOKKEEPER";
-            String project = "SYNCOPE";
-            //String path = "\\Users\\lucad\\Documents\\bookkeeper_fork";
-            String path = "\\Users\\lucad\\Documents\\syncope";
+            String project = "BOOKKEEPER";
+            //String project = "SYNCOPE";
+            String path = "\\Users\\lucad\\Documents\\bookkeeper_fork";
+            //String path = "\\Users\\lucad\\Documents\\syncope";
 
             List<Release> releases;
             List<Ticket> tickets;
