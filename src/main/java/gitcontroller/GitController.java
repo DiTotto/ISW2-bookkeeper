6872a02ffa3d09da7456d03f4c2d0443edb59a36
DiTotto <luca.ditotto99@gmail.com>
2025-01-07 12:27:35 +0100

    solving code smell in GitController

--- src/main/java/gitcontroller/GitController.java
+++ src/main/java/gitcontroller/GitController.java
@@ -1,6 +1,5 @@
 package gitcontroller;
 
-import common.Main;
 import models.Release;
 import models.FileJava;
 
@@ -86,46 +85,10 @@ public class GitController {
     //associo i commit alle release
     public static void associateCommitsWithReleases(List<Release> releases, String repoPath) {
         logger.log(java.util.logging.Level.INFO , "\u001B[37mAssociating commits with releases...\u001B[0m");
-        /*try (Repository repository = Git.open(new File(repoPath)).getRepository()) {
-            //l'istanza di Git permette di accedere a tutti i comandi di Git
-            try (Git git = new Git(repository)) {
-                //ottengo tutti i commit dal repository --> l'Iterable rappresenta una sequenza di commit
-                Iterable<RevCommit> commits = git.log().call();
-
-                for (RevCommit commit : commits) {
-                    for (Release release : releases) {
-                        //se il commit è stato effettuato prima della data della release
-                        if (commit.getCommitTime() * 1000L <= release.getReleaseDate().toInstant(ZoneOffset.UTC).toEpochMilli()) {
-                            //associo il commit alla release
-                            release.addCommit(commit);
-                        }
-                    }
-                }
-            }
-
-
-
-            //associo i file ad ogni releases
-            //associateFilesWithCommits(releases, repoPath);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
         List<RevCommit> commits = getAllCommits(repoPath);
 
         // BUGGY perche in questo modo inserisce in una certa release, tutti i commit associabili a quella release e i
         // commit associati alle release precedenti a livello di date
-
-        /*for (RevCommit commit : commits) {
-            for (Release release : releases) {
-                //se il commit è stato effettuato prima della data della release
-                long commitTime = commit.getCommitTime() * 1000L;
-                long releaseDate = release.getReleaseDate().toInstant(ZoneOffset.UTC).toEpochMilli();
-                if (commit.getCommitTime() * 1000L <= release.getReleaseDate().toInstant(ZoneOffset.UTC).toEpochMilli()) {
-                    //associo il commit alla release
-                    release.addCommit(commit);
-                }
-            }
-        }*/
 
         for (int i = 0; i < releases.size(); i++) {
             Release currentRelease = releases.get(i);
@@ -159,15 +122,11 @@ public class GitController {
     public static void associateFilesWithCommits(List<Release> releases, String repoPath) {
         logger.log(java.util.logging.Level.INFO, "\u001B[37mAssociating files with commits...\u001B[0m");
         try (Repository repository = Git.open(new File(repoPath)).getRepository()) {
-            //try (Git git = new Git(repository)) {
                 //ottengo tutti i commit dal repository
-                //Iterable<RevCommit> commits = git.log().call();
 
-                //for (RevCommit commit : commits) {
                     for (Release release : releases) {
                         for (RevCommit releaseCommit : release.getCommits()) {
                             //se il commit è incluso in quella release
-                            //if (commit.getId().equals(releaseCommit.getId())) {
                                 //se il commit è associato alla release
                                 //inizializzo un nuovo TreeWalk per navigare l'albero del commit
                                 try (TreeWalk treeWalk = new TreeWalk(repository)) {
@@ -177,16 +136,8 @@ public class GitController {
                                     while (treeWalk.next()) {
                                         //per ogni file, ottengo il nome
                                         String filePath = treeWalk.getPathString();
-                                        //System.out.println("Found file: " + filePath);
-                                        //controllo se è .java
-                                        //if (filePath.startsWith("bookkeeper/src/main")){
-                                        //     int index = filePath.indexOf("bookkeeper/src/main");
-                                        //     //System.out.println(filePath);
-                                        //     //break;
-                                        //
-                                        // }
+                                        //se il file è un file Java
                                         if (filePath.endsWith(".java")) {
-
                                             //controllo se è già presente nella lista dei file
                                             //non voglio duplicati
                                             boolean exists = false;
@@ -235,42 +186,6 @@ public class GitController {
 
     //calcolo in maniera cumulativa i LOC totali per ogni file Java per ogni release
     //i LOC sono calcolati tra una release e l'altra
-    /*private static void setFileLOCForCommit(Repository repo, RevCommit commit, List<FileJava> javaFiles) throws IOException, GitAPIException {
-
-        RevTree tree = commit.getTree();
-
-        for (FileJava javaFile : javaFiles) {
-            String filePath = javaFile.getName();
-
-            TreeWalk treeWalk = new TreeWalk(repo);
-            treeWalk.addTree(tree);
-            treeWalk.setRecursive(true);
-            treeWalk.setFilter(PathFilter.create(filePath));
-
-            if (!treeWalk.next()) {
-                continue;
-            }
-
-            ObjectId objectId = treeWalk.getObjectId(0);
-            ObjectLoader loader = repo.open(objectId);
-
-            //legge il contenuto del file e conta le LOC
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(loader.openStream()))) {
-                String line;
-                int numLines = 0;
-                while ((line = reader.readLine()) != null) {
-                    String trimmedLine = line.trim();
-                    if (!trimmedLine.isEmpty() && !(trimmedLine.startsWith("/*") ||
-                            trimmedLine.startsWith("*") || trimmedLine.startsWith("//"))) {
-                        numLines++;
-                    }
-                }
-                //aggiorna le LOC totali nel JavaFile
-                //maybe this should be only numLines
-                javaFile.setLoc(javaFile.getLoc() + numLines);
-            }
-        }
-    }*/
 
     private static void setFileLOCForCommit(Repository repo, RevCommit commit, List<FileJava> javaFiles) throws IOException, GitAPIException {
         try (Git git = new Git(repo)) {
@@ -350,12 +265,13 @@ public class GitController {
                         int parentCount = commit.getParentCount();
 
                         //per ogni commit itero sui file modificati
+                        DiffFormatter diffFormatter = new DiffFormatter(DisabledOutputStream.INSTANCE);
+                        diffFormatter.setRepository(repository);
+                        List<DiffEntry> diffs;
                         if (parentCount > 0) {
-                            DiffFormatter diffFormatter = new DiffFormatter(DisabledOutputStream.INSTANCE);
-                            diffFormatter.setRepository(repository);
 
                             //esegue diff tra commit attuale e primo genitore per capire quali file sono stati modificati
-                            List<DiffEntry> diffs = diffFormatter.scan(commit.getParent(0), commit);
+                            diffs = diffFormatter.scan(commit.getParent(0), commit);
 
                             //per ogni file .java modificato
                             for (DiffEntry diff : diffs) {
@@ -370,11 +286,9 @@ public class GitController {
                             }
                         } else {
                             //quando ho un commit senza genitore, controllo solo i file modificati
-                            DiffFormatter diffFormatter = new DiffFormatter(DisabledOutputStream.INSTANCE);
-                            diffFormatter.setRepository(repository);
 
                             //uso un albero vuoto per il confronto --> non ho genitori
-                            List<DiffEntry> diffs = diffFormatter.scan(null, commit);
+                            diffs = diffFormatter.scan(null, commit);
 
                             for (DiffEntry diff : diffs) {
                                 String filePath = diff.getNewPath();
