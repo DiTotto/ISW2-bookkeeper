bd938e9d91c52315d5075cd92e531ba0eb856986
DiTotto <luca.ditotto99@gmail.com>
2025-01-09 15:43:37 +0100

    modified getTicket method in JiraTicket

--- src/main/java/jiracontroller/JiraTicket.java
+++ src/main/java/jiracontroller/JiraTicket.java
@@ -8,10 +8,7 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 import java.util.regex.Pattern;
 
 import static jiracontroller.JiraRelease.*;
@@ -23,82 +20,109 @@ public class JiraTicket {
         throw new IllegalStateException("Utility class");
     }
 
+    private static final String FIELDS_KEY = "fields";
+
     public static List<Ticket> getTickets(String project, List<Release> releaseList) throws IOException {
         Integer j = 0, i = 0, total = 1;
         Integer injectionVersion = null;
 
         List<Ticket> tickets = new ArrayList<>();
 
-        HashMap<Integer, Release> releseIDMap = new HashMap<>();
+        //HashMap<Integer, Release> releseIDMap = new HashMap<>();
+        Map<Integer, Release> releaseIDMap = createReleaseIDMap(project, releaseList);
+        int totalIssues = fetchAndProcessIssues(project, releaseIDMap, tickets);
+
+
+        processTickets(tickets, releaseList);
+
+        return tickets;
+    }
+
+    // Metodo per creare la mappa delle release
+    private static Map<Integer, Release> createReleaseIDMap(String project, List<Release> releaseList) throws IOException {
         if (releaseList == null) {
             releaseList = getRelease(project);
         }
 
         //inserisco tutte le release nella mappa e uso come chiave il loro id,
         //utile per recuperare le AV
-
-        for (Release relese : releaseList) {
-            releseIDMap.put(relese.getId(), relese);
+        Map<Integer, Release> releaseIDMap = new HashMap<>();
+        for (Release release : releaseList) {
+            releaseIDMap.put(release.getId(), release);
         }
+        return releaseIDMap;
+    }
 
+    // Metodo per recuperare e processare i bug
+    private static int fetchAndProcessIssues(String project, Map<Integer, Release> releaseIDMap, List<Ticket> tickets) throws IOException {
+        int total = 1, i = 0;
 
         //Get JSON API for closed bugs w/ AV in the project
         do {
             //Only gets a max of 1000 at a time, so must do this multiple times if bugs >1000
-            j = i + 1000;
-            String url = "https://issues.apache.org/jira/rest/api/2/search?jql=project=%22"
-                    + project + "%22AND%22issueType%22=%22Bug%22AND(%22status%22=%22closed%22OR"
-                    + "%22status%22=%22resolved%22)AND%22resolution%22=%22fixed%22&fields=key,resolutiondate,versions,created&startAt="
-                    + i.toString() + "&maxResults=" + j.toString();
+            int j = i + 1000;
+            String url = buildJiraQueryURL(project, i, j);
             JSONObject json = readJsonFromUrl(url);
             JSONArray issues = json.getJSONArray("issues");
             total = json.getInt("total");
-            for (; i < total && i < j; i++) {
-                //Iterate through each bug
-                //String key = issues.getJSONObject(i%1000).get("key").toString();
-                //System.out.println(key);
 
-                JSONObject issue = issues.getJSONObject(i % 1000);
-                String key = issue.get("key").toString();
-
-
-
-                LocalDateTime openDate = LocalDateTime.parse(issue.getJSONObject("fields").getString("created").substring(0, 16));
-                LocalDateTime resolutionDate = LocalDateTime.parse(issue.getJSONObject("fields").getString("resolutiondate").substring(0, 16));
-                JSONArray affectedVersions = issue.getJSONObject("fields").getJSONArray("versions");
-
-                injectionVersion = null;
-
-                if(affectedVersions.length() > 0) {
-                    //abbiamo informazioni sulle AV --> la prima AV è presumibilmente la IV
-                    injectionVersion = affectedVersions.getJSONObject(0).getInt("id");
-                    //System.out.println("inj: " + injectionVersion);
-                }
-
-                if (affectedVersions.length() > 0) {
-                    //se ho AV allora la IV sarà la prima versione
-                    injectionVersion = affectedVersions.getJSONObject(0).getInt("id");
-                    //l'id non ci va bene, a noi serve l'indice (1, 2, ...)
-                    if(releseIDMap.containsKey(injectionVersion)) {
-                        injectionVersion = releseIDMap.get(injectionVersion).getIndex();
-                    }
-                }
-
-                Ticket ticket = new Ticket(key, openDate, resolutionDate, injectionVersion);
+            //Iterate through each bug
+            for (int index = i; index < total && index < j; index++) {
+                JSONObject issue = issues.getJSONObject(index % 1000);
+                Ticket ticket = createTicketFromIssue(issue, releaseIDMap);
                 tickets.add(ticket);
-
             }
+
+            i = j;
         } while (i < total);
 
-        for(Ticket ticket: tickets) {
+        return total;
+    }
+
+    // Metodo per costruire la URL di query
+    private static String buildJiraQueryURL(String project, int startAt, int maxResults) {
+        return String.format(
+                "https://issues.apache.org/jira/rest/api/2/search?jql=project=%%22%s%%22AND%%22issueType%%22=%%22Bug%%22AND(%%22status%%22=%%22closed%%22OR"
+                        + "%%22status%%22=%%22resolved%%22)AND%%22resolution%%22=%%22fixed%%22&fields=key,resolutiondate,versions,created&startAt=%d&maxResults=%d",
+                project, startAt, maxResults
+        );
+    }
+
+    // Metodo per creare un oggetto Ticket da un JSONObject
+    private static Ticket createTicketFromIssue(JSONObject issue, Map<Integer, Release> releaseIDMap) {
+        String key = issue.get("key").toString();
+        LocalDateTime openDate = LocalDateTime.parse(issue.getJSONObject(FIELDS_KEY).getString("created").substring(0, 16));
+        LocalDateTime resolutionDate = LocalDateTime.parse(issue.getJSONObject(FIELDS_KEY).getString("resolutiondate").substring(0, 16));
+        Integer injectionVersion = getInjectionVersion(issue, releaseIDMap);
+
+        return new Ticket(key, openDate, resolutionDate, injectionVersion);
+    }
+
+    // Metodo per recuperare la injection version
+    private static Integer getInjectionVersion(JSONObject issue, Map<Integer, Release> releaseIDMap) {
+        JSONArray affectedVersions = issue.getJSONObject(FIELDS_KEY).getJSONArray("versions");
+        if (affectedVersions.length() > 0) {
+            //abbiamo informazioni sulle AV --> la prima AV è presumibilmente la IV
+            //se ho AV allora la IV sarà la prima versione
+            //l'id non ci va bene, a noi serve l'indice (1, 2, ...)
+            Integer injectionVersionID = affectedVersions.getJSONObject(0).getInt("id");
+            if (releaseIDMap.containsKey(injectionVersionID)) {
+                //l'id non ci va bene, a noi serve l'indice (1, 2, ...)
+                return releaseIDMap.get(injectionVersionID).getIndex();
+            }
+        }
+        return null;
+    }
+
+    // Metodo per processare i ticket
+    private static void processTickets(List<Ticket> tickets, List<Release> releaseList) {
+        for (Ticket ticket : tickets) {
             ticket.setOpeningVersion(getOV(ticket, releaseList));
             ticket.setFixedVersion(getFV(ticket, releaseList));
-            //System.out.println("Tickets: " + ticket);
         }
-
-
-        return tickets;
     }
+
+
 
     public static Integer getOV(Ticket ticket, List<Release> releases) {
         //per ogni release, se la data dell'apertura del ticket è più piccola della data della release,
